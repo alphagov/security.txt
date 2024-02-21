a2721bf2e82219d8e75d9ad587c32a7d1ce23157
Bart Labno <bart.labno@digital.cabinet-office.gov.uk>
2024-02-21 12:04:32 +0000

    Debug output for the secuiry.txt

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATE_NOW=$(date --rfc-3339='seconds')
-DATE_EXP=$(date -d '+3 months' ${DATE_NOW})
+DATE_EXP=$(date -d '+3 months' --rfc-3339='seconds')
 mkdir -p ./paas_app/.well-known/
 cp ./security.txt ./paas_app/security.txt
 cp ./thanks.txt ./paas_app/thanks.txt
