e101f450a2637f6f516c26673cc85b2cbd3bea17
Bart Labno <bart.labno@digital.cabinet-office.gov.uk>
2024-02-21 12:21:10 +0000

    Improve script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-DATE_NOW=$(date --rfc-3339='seconds')
-DATE_EXP=$(date -d "+3 months" ${DATE_NOW})
-mkdir -p ./paas_app/.well-known/
-cp ./security.txt ./paas_app/security.txt
-cp ./thanks.txt ./paas_app/thanks.txt
-sed -i "s/DATE_NOW/${DATE_NOW}/g" ./paas_app/security.txt
-sed -i "s/DATE_EXP/${DATE_EXP}/g" ./paas_app/security.txt
-cp ./paas_app/security.txt ./paas_app/.well-known/security.txt
+# Manage content on the S3 for prod account.
 
-# DEBUG
-cat ./paas_app/security.txt
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    DATE_NOW=$(date --rfc-3339='seconds')
+    DATE_EXP=$(date -d "+3 months" --rfc-3339='seconds')
+
+    sed -i "s/DATE_NOW/${DATE_NOW}/g" ./security.txt
+    sed -i "s/DATE_EXP/${DATE_EXP}/g" ./security.txt
+
+    mkdir -p ./paas_app/.well-known/
+    cp ./security.txt ./paas_app/security.txt
+    cp ./thanks.txt ./paas_app/thanks.txt
+    cp ./paas_app/security.txt ./paas_app/.well-known/security.txt
+
+    aws s3 cp --recursive paas_app s3://prod-vdp.cabinetoffice.gov.uk-website
+else
+    echo "Due to different output format in date, only Linux is supported"
+    exit 1
+fi
