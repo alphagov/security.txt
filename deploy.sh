a7d5ead5e436b0d843c4192c3d93c65f312d1acc
Bart Labno <bart.labno@digital.cabinet-office.gov.uk>
2024-02-21 12:09:30 +0000

    Variable for date_exp in for the secuiry.txt

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-date -d "+3 months" --rfc-3339='seconds'
 DATE_NOW=$(date --rfc-3339='seconds')
-DATE_EXP=$(date -d "+3 months" --rfc-3339='seconds')
+DATE_EXP=$(date -d "+3 months" ${DATE_NOW})
 mkdir -p ./paas_app/.well-known/
 cp ./security.txt ./paas_app/security.txt
 cp ./thanks.txt ./paas_app/thanks.txt
 sed -i "s/DATE_NOW/${DATE_NOW}/g" ./paas_app/security.txt
-sed -i "s/DATE_EXP/${DATE_EXP}/g" ./paas/app/security.txt
+sed -i "s/DATE_EXP/${DATE_EXP}/g" ./paas_app/security.txt
 cp ./paas_app/security.txt ./paas_app/.well-known/security.txt
 
 # DEBUG
